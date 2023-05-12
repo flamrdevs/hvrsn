56e7db62b9898ee9bdea6abc468042403506796b
flamrdevs <flamrdevs@gmail.com>
2023-05-13 05:07:44 +0700

    chore: hvrsn

---
+++ src/index.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest";
+
+import hvrsn from ".";
+
+describe("hvrsn", () => {
+  it("hvrsn", () => {
+    expect(hvrsn).toBeTypeOf("function");
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-const hvrsn = () => {
-  return "hvrsn";
-};
+const R = 6371;
+const sqr = (v: number) => v * v;
+const c = (a: number) => 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
+const hvrsn = (radlat1: number, radlng1: number, radlat2: number, radlng2: number, r: number = R) =>
+  r * c(sqr(Math.sin((radlat2 - radlat1) / 2)) + Math.cos(radlat1) * Math.cos(radlat2) * sqr(Math.sin((radlng2 - radlng1) / 2)));
+
+export { R };
 export default hvrsn;
